3a5fed7f903bdd9c0ffe422c1eaf3aedbbcff13f
ridamayanti <ridamayantikuliah@gmail.com>
2021-11-19 23:13:13 +0700

    Pertemuan 10

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -16,67 +16,66 @@
             Data Barang Tidak Ditemukan...
         </div>
     <?php endif; ?>
-
-    <div class="row-mt-2">
-        <div class="colmd-4">
+    <div class="row-mt-3">
+        <div class="colmd-4"><br>
             <a href="<?= base_url(); ?>barang/tambah" class="btn btn-primary">Tambahkan Data Barang</a>
         </div>
-
-        <br>
-        <div class="row-mt-3">
-            <div class="col-md-6">
-                <form action="" method="post">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Cari Data Barang..." name="keyword">
-                        <button class="btn btn-primary" type="submit">Cari</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <!-- DataTales Example -->
-        <br>
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Kode Barang</th>
-                                <th scope="col">Nama Barang</th>
-                                <th scope="col">Harga</th>
-                                <th scope="col">Stock</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <?php foreach ($barang as $brg) : ?>
-                                    <td><?= ++$start; ?></td>
-                                    <td><?= $brg['id_barang']; ?></td>
-                                    <td><?= $brg['nama_barang']; ?></td>
-                                    <td><?= $brg['harga']; ?></td>
-                                    <td><?= $brg['stock']; ?></td>
-                                    <td> <a href="<?= base_url(); ?>barang/detail/<?= $brg['id_barang']; ?>" class="btn btn-primary">Detail</a>
-                                        <a href="<?= base_url(); ?>barang/ubah/<?= $brg['id_barang']; ?>" class="btn btn-success">Ubah</a>
-                                        <a href="<?= base_url(); ?>barang/hapus/<?= $brg['id_barang']; ?>" class="btn btn-danger" onclick="return confirm ('Apakah Yakin Akan Dihapus?')">Hapus</a>
-                                    </td>
-                            </tr>
-                        <?php endforeach ?>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <?= $this->pagination->create_links(); ?>
-                </div>
-            </div>
-        </div>
-
     </div>
-    <!-- /.container-fluid -->
+    <br>
+    <div class="row mt3">
+        <div class="col-md-6">
+            <form action="" method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cari Data Barang..." name="keyword">
+                    <button class="btn btn-primary" type="submit">Cari</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- DataTales Example -->
+    <br>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Kode Barang</th>
+                            <th scope="col">Nama Barang</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <?php foreach ($barang as $brg) : ?>
+                                <td><?= ++$start; ?></td>
+                                <td><?= $brg['id_barang']; ?></td>
+                                <td><?= $brg['nama_barang']; ?></td>
+                                <td><?= $brg['harga']; ?></td>
+                                <td><?= $brg['stock']; ?></td>
+                                <td> <a href="<?= base_url(); ?>barang/detail/<?= $brg['id_barang']; ?>" class="btn btn-primary">Detail</a>
+                                    <a href="<?= base_url(); ?>barang/ubah/<?= $brg['id_barang']; ?>" class="btn btn-success">Ubah</a>
+                                    <a href="<?= base_url(); ?>barang/hapus/<?= $brg['id_barang']; ?>" class="btn btn-danger" onclick="return confirm ('Apakah Yakin Akan Dihapus?')">Hapus</a>
+                                </td>
+                        </tr>
+                    <?php endforeach ?>
+                    </tr>
+                    </tbody>
+                </table>
+                <?= $this->pagination->create_links(); ?>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
